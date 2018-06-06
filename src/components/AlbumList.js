a12106efc081c6469fb687362c44e01988daad41
Crystal Maria <crystalmaria@Crystals-MacBook-Air.local>
2018-06-06 15:12:08 -0400

    first commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+  //inital state === this.state.albums
+  state = { albums: [] };
+
+ // set new state
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
+  render() {
+  return (
+    <View>
+      {this.renderAlbums()}
+    </View>
+    );
+  }
+}
+
+export default AlbumList;
